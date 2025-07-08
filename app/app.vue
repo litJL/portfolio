150b8c587b56861c1a5448b35f5b49f2dde6ff29
Mike Lenz <lenz@cando-solutions.com>
2025-07-08 23:45:15 +0200

    feat: set up modules and packages

---
+++ app/app.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <NuxtRouteAnnouncer />
+        <UApp>
+            <div class="w-50 bg-red-500">{{ $t("hello_world") }}</div>
+        </UApp>
+    </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+    compatibilityDate: "2025-05-15",
+    future: {
+        compatibilityVersion: 4,
+    },
+    css: ["~/assets/css/main.css"],
+    devtools: { enabled: true },
+    modules: ["@nuxt/ui", "@nuxtjs/i18n", "@nuxt/eslint", "@vueuse/nuxt"],
+
+    colorMode: {
+        preference: "dark",
+        fallback: "dark",
+    },
+
+    imports: {
+        dirs: ["./types"],
+    },
+
+    i18n: {
+        bundle: {
+            optimizeTranslationDirective: false,
+        },
+        locales: [
+            { code: "de", name: "Deutsch", file: "de.json" },
+            { code: "en", name: "English", file: "en.json" },
+        ],
+        defaultLocale: "de",
+    },
+});
